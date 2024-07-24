de999f0b74836b0581f68b4cb468282884066628
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-07-25 00:20:18 +0530

    display data

--- diary/app/page.tsx
+++ diary/app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+'use client';
+import React, { useContext } from 'react';
+import Image from 'next/image';
+import { DataContext, DataContextType } from './context/DataContext';
 
-export default function Home() {
+const Home: React.FC = () => {
+  const context = useContext<DataContextType | undefined>(DataContext);
+
+  if (!context) {
+    throw new Error('DataContext must be used within a DataContextProvider');
+  }
+
+  const { data } = context;
+
   return (
-    <main className="flex w-[100%] h-screen flex-col items-center justify-between p-24 bg-neutral-900">
-    <div></div>
+    <main className="flex w-[100%] h-screen flex-col text-white items-center justify-between p-24 bg-neutral-900">
+      <div>
+        {data.map((item, index) => (
+          <div key={index}>
+            <div>{item.title}</div>
+            <div>{item.desp}</div>
+            <div>{item.category}</div>
+          </div>
+        ))}
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
